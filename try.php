74a65a8c1f9148b217bc5fe69f7c3665ee0d5899
mara <maragatchalian@github.com>
2015-01-30 14:52:07 +0800

    Ch4 - Heredoc

--- try.php
+++ try.php
@@ -6,5 +6,15 @@ $where = 'here';
 echo "$who was $where" ."<br>";
 
 $n = 12;
-echo  "You are the {$n}th person"; 
+echo  "You are the {$n}th person" ."<br>" ."<br>"; 
+
+//Here Documents
+$clerihew = <<< EndOfQuote
+Sir Humphrey Davy
+Abominated gravy.
+He lived in the odium
+Of having discovered sodium.
+EndOfQuote;
+echo $clerihew;
+
 ?> 
