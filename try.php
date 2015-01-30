849a5df388f4fef0940de7f3cb4434ee37e94cc5
mara <maragatchalian@github.com>
2015-01-30 16:20:40 +0800

    Ch4 - Printing Strings

--- try.php
+++ try.php
@@ -15,6 +15,20 @@ Abominated gravy.
 He lived in the odium
 Of having discovered sodium.
 EndOfQuote;
-echo $clerihew;
+echo $clerihew ."<br>" ."<br>";
 
-?> 
+//Printing Strings
+#1 Echo
+echo "Hello, World!" ."<br>";
+echo ("Hello, World!" ."<br>");
+/*Because echo is a true fuction,
+we cannot us it as part of a larger expression*/
+
+#2 Print()
+if (print("test" ."<br>")) {
+print("It worked!" ."<br>");
+}
+
+#3 Printf
+printf('Bond. James Bond. %03d.', 7);
+
